c95f526d2bfc5a8585fd2b75c9eddd4f150d0aa0
Adrián Díaz <onelifedesigning@gmail.com>
2017-11-24 14:02:21 +0100

    fix js

--- public/js/scaninstascan.js
+++ public/js/scaninstascan.js
@@ -1,59 +1,68 @@
-let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+var scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
 function startScan(){
-    scanner.addListener('instascan', function (content) {
+    scanner.addListener('scan', function (content) {
+        console.log(content);
         if (content.length !== 0) {
-            $.ajax({
-                type: "GET",
-                url: "/registration/verify/"+ content,
-                dataType: "json",
-                cache: false,
-                success: function(a) {
-                    if (a.status == 'error') {
-                        type = BootstrapDialog.TYPE_DANGER;
-                        title = 'Error';
-                        message = '<h4>'+a.message+'</h4>';
-                        buttons = [{
-                            label: '<i class="fa fa-times-circle" aria-hidden="true"></i> Cerrar',
-                            cssClass: 'btn-primary btn-lg',
-                            action: function(dialog) {
-                                dialog.close();
-                            }
-                        }];
-                    } else if (a.status == 'success') {
-                    console.log(a);
-                        type = BootstrapDialog.TYPE_SUCCESS;
-                        title = a.message;
-                        message = '<h4>'+a.user+'<br><small>'+a.type+'</small></h4>';
-                        buttons = [{
-                            label: '<i class="fa fa-times-circle" aria-hidden="true"></i> Cerrar',
-                            cssClass: 'btn-primary btn-lg',
-                            action: function(dialog) {
-                                dialog.close();
-                            }
-                        }];
-                        $(this).data('uniqueid', null)
-                            .removeClass('text-danger')
-                            .addClass('text-success')
-                            .children('i')
-                            .removeClass('fa-times-circle')
-                            .addClass('fa-check-circle');
-                    }
-                    BootstrapDialog.show({
-                        size: BootstrapDialog.SIZE_LARGE,
-                        title: title,
-                        message: message,
-                        buttons: buttons,
-                        type: type,
-                        onhide: function(dialogRef) {
-                            decode = true;
+        $.ajax({
+            type: "GET",
+            url: "/registration/verify/"+ content,
+            dataType: "json",
+            cache: false,
+            success: function(a) {
+                if (a.status == 'error') {
+                    type = BootstrapDialog.TYPE_DANGER;
+                    title = 'Error';
+                    message = '<h4>'+a.message+'</h4>';
+                    buttons = [{
+                        label: '<i class="fa fa-times-circle" aria-hidden="true"></i> Cerrar',
+                        cssClass: 'btn-primary btn-lg',
+                        action: function(dialog) {
+                            dialog.close();
                         }
-                    });
-                },
-                error: function(a) {
-                    location.reload();
+                    }];
+                } else if (a.status == 'success') {
+                    type = BootstrapDialog.TYPE_SUCCESS;
+                    title = a.message;
+                    message = '<h4>'+a.user+'</h4>';
+                    buttons = [{
+                        label: '<i class="fa fa-times-circle" aria-hidden="true"></i> Cerrar',
+                        cssClass: 'btn-primary btn-lg',
+                        action: function(dialog) {
+                            dialog.close();
+                        }
+                    }];
+                    $(this).data('uniqueid', null)
+                        .removeClass('text-danger')
+                        .addClass('text-success')
+                        .children('i')
+                        .removeClass('fa-times-circle')
+                        .addClass('fa-check-circle');
                 }
-            });
+                BootstrapDialog.show({
+                    size: BootstrapDialog.SIZE_LARGE,
+                    title: title,
+                    message: message,
+                    buttons: buttons,
+                    type: type,
+                    onhide: function(dialogRef) {
+                        decode = true;
+                    }
+                });
+            },
+            error: function(a) {
+                location.reload();
+            }
+        });
         }
+    });
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+          if(cameras[1]){ scanner.start(cameras[1]); } else { scanner.start(cameras[0]); } 
+        } else {
+          console.error('No cameras found.');
+        }
+    }).catch(function (e) {
+        console.error(e);
     });
 };
 function stopScan(){
