c8dd6730992a0c7868a9baca710635dfd023ab3c
Adrián Díaz <onelifedesigning@gmail.com>
2017-11-24 14:30:07 +0100

    video res

--- public/js/scaninstascan.js
+++ public/js/scaninstascan.js
@@ -1,3 +1,37 @@
+
+
+
+$(document).ready(function(){
+  
+    var vid = document.getElementById("preview");
+    var width, height;
+
+    vid_w_orig = parseInt(vid.style.width);
+    vid_h_orig = parseInt(vid.style.height);
+    
+    // re-scale image when viewport resizes
+    $(window).resize(function(){
+    
+        // get the parent element size
+        var container_w = vid.parent().width();
+        var container_h = vid.parent().height();
+    
+        // use largest scale factor of horizontal/vertical
+        var scale_w = container_w / vid_w_orig;
+        var scale_h = container_h / vid_h_orig;
+        var scale = scale_w > scale_h ? scale_w : scale_h;
+    
+        // scale the video
+        vid.width(scale * vid_w_orig);
+        vid.height(scale * vid_h_orig);
+    
+    });
+  
+  // trigger re-scale on pageload
+    $(window).trigger('resize');
+  
+});
+
 var scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
 function startScan(){
     scanner.addListener('scan', function (content) {
