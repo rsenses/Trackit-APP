77a67734941aaf5464ea43f836f3a8a587d55662
Rubén Silva <me@rsenses.com>
2018-03-01 17:19:23 +0100

    deployer

--- deploy.php
+++ deploy.php
@@ -55,7 +55,7 @@ set('keep_releases', 2);
 
 task('reload:server', function () {
     run('/etc/init.d/nginx reload');
-    run('/etc/init.d/php7.0-fpm restart');
+    run('/etc/init.d/php7.1-fpm restart');
 });
 
 after('deploy', 'reload:server');
