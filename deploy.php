fc69a8a2d9edfd747cc418fb6c28868e80dab8ff
Adrián Díaz <adrian.diaz@expomark.es>
2018-09-12 09:29:53 +0200

    add dep test

--- deploy.php
+++ deploy.php
@@ -31,6 +31,13 @@ host('scl.prs3.expomark.es')
     ->stage('production')
     ->set('deploy_path', '/var/www/app.trackitsuite.com'); // Define the base path to deploy your project to.
 
+// Define a server for deployment.
+host('expobeta.com')
+    ->user('deploy')
+    // ->forwardAgent() // You can use identity key, ssh config, or username/password to auth on the server.
+    ->stage('testing')
+    ->set('deploy_path', '/var/www/app.trackitsuite.com'); // Define the base path to deploy your project to.
+
 // Specify the repository from which to download your project's code.
 // The server needs to have git installed for this to work.
 // If you're not using a forward agent, then the server has to be able to clone
