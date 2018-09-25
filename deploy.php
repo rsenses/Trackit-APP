77e76e4ef442700c1693eb5d9cd0aac2e603a054
Adrián Díaz <adrian.diaz@expomark.es>
2018-09-25 13:09:38 +0200

    count for

--- deploy.php
+++ deploy.php
@@ -29,7 +29,8 @@ host('scl.prs3.expomark.es')
     ->identityFile('~/.ssh/do_rsa')
     // ->forwardAgent() // You can use identity key, ssh config, or username/password to auth on the server.
     ->stage('production')
-    ->set('deploy_path', '/var/www/app.trackitsuite.com'); // Define the base path to deploy your project to.
+    ->set('deploy_path', '/var/www/app.trackitsuite.com') // Define the base path to deploy your project to.
+    ->set('branch', 'master');
 
 // Define a server for deployment.
 host('expobeta.com')
